84d668996dc32a2f193b9ab108c0b455d1bde41e
pshoxsanam2197 <pshoxsanam2197@gmail.com>
2025-11-19 15:30:49 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,73 @@
+#1-misol
+s = "Python2025"
+print(s)
+
+toza = s[-4:]
+print(toza)
+
+# 2-misol
+s = 'abrakadabra'
+print(s)
+
+toza = s[5:8]
+print(toza)
+
+toza = s[5:8]
+print(toza)
+
+# 3-misol
+s = "HelloWorldWideWeb"
+print(s)
+
+toza = s[5:10]
+print(toza)
+
+# 4-misol
+s = '123abc456def789'
+print(s)
+
+toza = s[::-1]
+print(toza)
+
+# 5-misol
+s = 'ProgrammingFun'
+print(s)
+
+toza = s[::-1]
+print(toza)
+
+# 6-misol
+s = ('DataScience2025')
+print(s)
+
+toza = s[4:-4]
+print(toza)
+
+# 7-misol
+s = "abcdefghij"
+print(s)
+
+toza = s[2:8:2]+s[7:1:-1]
+print(toza)
+
+# 8-misol
+s = "Xursandchilik"
+print(s)
+
+toza = s[3:7]
+print(toza)
+
+# 9-misol
+s = "Al_Revolution_2030"
+print(s)
+
+toza = s[-4:]
+print(toza)
+
+# 10-misol
+s = "qwertyuiop"
+print(s)
+
+toza = s[::-1]
+natija = toza[::2]
+print(natija)
